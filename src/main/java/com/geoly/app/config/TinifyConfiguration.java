0f9588e75e1ae25326e7df1fdc7fccf7987f1415
DuS4NN <34943373+DuS4NN@users.noreply.github.com>
2020-07-29 11:48:05 +0200

    Add Tinify

---
+++ src/main/java/com/geoly/app/config/TinifyConfiguration.java
@@ -0,0 +1,12 @@
+package com.geoly.app.config;
+
+import com.tinify.*;
+import org.springframework.stereotype.Component;
+
+@Component
+public class TinifyConfiguration {
+
+    public TinifyConfiguration(){
+        Tinify.setKey("yKpphVQPqK9nx418D2f2QRMPfktYtp6R");
+    }
+}
