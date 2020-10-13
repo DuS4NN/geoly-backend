44af0099adee0e396b39738edd092ced7a65a440
DuS4NN <34943373+DuS4NN@users.noreply.github.com>
2020-10-13 12:40:56 +0200

    Add secret keys in .properties file

--- src/main/java/com/geoly/app/config/SentryAPI.java
+++ src/main/java/com/geoly/app/config/SentryAPI.java
@@ -3,14 +3,19 @@ package com.geoly.app.config;
 import io.sentry.Sentry;
 import io.sentry.SentryClient;
 import io.sentry.SentryClientFactory;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
 public class SentryAPI {
 
+
+    @Value("${sentry.url}")
+    private String url;
+
     public SentryAPI(){
-        Sentry.init("https://9abf2d4b51ca44328a41622d49247e21@o421143.ingest.sentry.io/5340459");
+        Sentry.init(url);
     }
 
     @Bean
--- src/main/java/com/geoly/app/config/TinifyConfiguration.java
+++ src/main/java/com/geoly/app/config/TinifyConfiguration.java
@@ -1,12 +1,16 @@
 package com.geoly.app.config;
 
 import com.tinify.*;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
 
 @Component
 public class TinifyConfiguration {
 
+    @Value("${tinify.key}")
+    private String key;
+
     public TinifyConfiguration(){
-        Tinify.setKey("yKpphVQPqK9nx418D2f2QRMPfktYtp6R");
+        Tinify.setKey(key);
     }
 }
