1ea2851b4399cab893c2d164b7c78ba4a2470ef8
DuS4NN <34943373+DuS4NN@users.noreply.github.com>
2020-11-07 12:31:22 +0100

    Refactor CustomAuthenticationSuccessHandler

--- src/main/java/com/geoly/app/config/AuthenticationHandler/CustomAuthenticationSuccessHandler.java
+++ src/main/java/com/geoly/app/config/AuthenticationHandler/CustomAuthenticationSuccessHandler.java
@@ -1,43 +1,72 @@
 package com.geoly.app.config.AuthenticationHandler;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
+import com.geoly.app.config.API;
+import com.geoly.app.jooq.tables.Role;
+import com.geoly.app.jooq.tables.User;
+import com.geoly.app.jooq.tables.UserOption;
+import com.geoly.app.jooq.tables.UserRole;
 import com.geoly.app.models.CustomUserDetails;
-import com.geoly.app.models.UserOption;
-import com.geoly.app.repositories.UserOptionRepository;
+import org.jooq.DSLContext;
+import org.jooq.Select;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.web.authentication.AuthenticationSuccessHandler;
 import org.springframework.stereotype.Component;
 
+import javax.persistence.EntityManager;
+import javax.persistence.Query;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import javax.transaction.Transactional;
 import java.io.IOException;
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
-import java.util.Optional;
 
 @Component
 public class CustomAuthenticationSuccessHandler implements AuthenticationSuccessHandler {
 
     private ObjectMapper objectMapper = new ObjectMapper();
     @Autowired
-    private UserOptionRepository userOptionRepository;
+    private EntityManager entityManager;
+    @Autowired
+    private DSLContext create;
 
-    @Override
+
+    @Transactional
     public void onAuthenticationSuccess(HttpServletRequest request, HttpServletResponse response, Authentication authentication) throws IOException {
         response.setStatus(HttpStatus.ACCEPTED.value());
         Map<String, Object> data = new HashMap<>();
 
         CustomUserDetails customUserDetails = (CustomUserDetails) authentication.getPrincipal();
-        Optional<UserOption> userOption = userOptionRepository.findByUser(customUserDetails.getUser());
-        if(!userOption.isPresent()) return;
 
-        data.put("nickName", customUserDetails.getUser().getNickName());
-        data.put("profileImage", customUserDetails.getUser().getProfileImageUrl());
-        data.put("mapTheme", userOption.get().getMapTheme());
-        data.put("darkMode", userOption.get().isDarkMode());
-        data.put("languageId", userOption.get().getLanguage().getId());
+        Select<?> roles =
+            create.select(Role.ROLE.NAME)
+                .from(UserRole.USER_ROLE)
+                .leftJoin(Role.ROLE)
+                    .on(Role.ROLE.ID.eq(UserRole.USER_ROLE.ROLE_ID))
+                .where(UserRole.USER_ROLE.USER_ID.eq(customUserDetails.getUser().getId()));
+
+        Query q1 = entityManager.createNativeQuery(roles.getSQL());
+        API.setBindParameterValues(q1, roles);
+        List rolesResult = q1.getResultList();
+
+        Select<?> options =
+            create.select(UserOption.USER_OPTION.LANGUAGE_ID, UserOption.USER_OPTION.MAP_THEME, UserOption.USER_OPTION.DARK_MODE, User.USER.NICK_NAME, User.USER.PROFILE_IMAGE_URL)
+                .from(UserOption.USER_OPTION)
+                .leftJoin(User.USER)
+                    .on(User.USER.ID.eq(UserOption.USER_OPTION.USER_ID))
+                .where(UserOption.USER_OPTION.USER_ID.eq(customUserDetails.getUser().getId()));
+
+        Query q2 = entityManager.createNativeQuery(options.getSQL());
+        API.setBindParameterValues(q2, options);
+        List optionsResult = q2.getResultList();
+
+        data.put("roles", rolesResult);
+        data.put("options", optionsResult);
+
         response.getOutputStream()
                 .println(objectMapper.writeValueAsString(data));
     }
