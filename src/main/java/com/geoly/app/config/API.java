93ced2cfedfb916cc236d2e4c4970cfa796c41f0
DuS4NN <34943373+DuS4NN@users.noreply.github.com>
2020-11-21 11:07:14 +0100

    Add uploadImage to Azure method

--- src/main/java/com/geoly/app/config/API.java
+++ src/main/java/com/geoly/app/config/API.java
@@ -2,7 +2,10 @@ package com.geoly.app.config;
 
 import com.geoly.app.dao.Response;
 import com.geoly.app.models.StatusMessage;
+import com.microsoft.azure.storage.blob.CloudBlobContainer;
+import com.microsoft.azure.storage.blob.CloudBlockBlob;
 import io.sentry.Sentry;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.http.HttpStatus;
 import org.springframework.mail.javamail.JavaMailSenderImpl;
 import org.springframework.mail.javamail.MimeMessageHelper;
@@ -17,13 +20,19 @@ import java.util.List;
 @Component
 public class API {
 
-    private JavaMailSenderImpl mailSender;
-    public static String questImageUrl = "static/images/quest/";
-    public static String userImageUrl = "static/images/user/";
-    public static String qrCodeImageUrl = "static/images/qrCode";
+    public static String questImageUrl = "quest/";
+    public static String userImageUrl = "user/";
+    public static String qrCodeImageUrl = "qrCode";
 
-    public API(JavaMailSenderImpl mailSender) {
+    @Value("${azure.images_link}")
+    private String imagesLink;
+
+    private JavaMailSenderImpl mailSender;
+    private CloudBlobContainer cloudBlobContainer;
+
+    public API(JavaMailSenderImpl mailSender, CloudBlobContainer cloudBlobContainer) {
         this.mailSender = mailSender;
+        this.cloudBlobContainer = cloudBlobContainer;
     }
 
     public static void setBindParameterValues(Query hibernateQuery, org.jooq.Query jooqQuery){
@@ -52,6 +61,19 @@ public class API {
         }catch (Exception e){
             Sentry.capture(e);
             e.printStackTrace();
+        }
+    }
+
+    public String uploadImage(String url, byte[] file){
+        CloudBlockBlob image = null;
+        try{
+            image = cloudBlobContainer.getBlockBlobReference(url);
+            image.uploadFromByteArray(file, 0, file.length);
+            return image.getUri().toString().replace(imagesLink, "");
+        }catch (Exception e){
+            Sentry.capture(e);
+            e.printStackTrace();
+            return null;
         }
     }
 }
