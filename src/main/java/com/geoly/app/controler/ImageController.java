3c070fb3114855886fdb71586213d9dae9043f76
DuS4NN <34943373+DuS4NN@users.noreply.github.com>
2020-11-21 10:31:24 +0100

    Add load images from Azure method

--- src/main/java/com/geoly/app/controler/ImageController.java
+++ src/main/java/com/geoly/app/controler/ImageController.java
@@ -1,5 +1,7 @@
 package com.geoly.app.controler;
 
+import com.microsoft.azure.storage.blob.*;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -8,18 +10,30 @@ import org.springframework.web.bind.annotation.RestController;
 
 import javax.imageio.ImageIO;
 import java.awt.image.BufferedImage;
-import java.io.ByteArrayOutputStream;
-import java.io.File;
+import java.io.*;
+import java.net.URL;
 
 @RestController
 public class ImageController {
+
+    private CloudBlobContainer cloudBlobContainer;
+    private CloudBlobClient cloudBlobClient;
+    @Value("${azure.sas}")
+    private String sasToken;
+    @Value("${azure.images_link}")
+    private String imagesLink;
+
+    public ImageController(CloudBlobContainer cloudBlobContainer, CloudBlobClient cloudBlobClient) {
+        this.cloudBlobContainer = cloudBlobContainer;
+        this.cloudBlobClient = cloudBlobClient;
+    }
 
     @GetMapping(value = "/image", produces = MediaType.IMAGE_JPEG_VALUE)
     public @ResponseBody
     byte[] getImage(@RequestParam(name = "url") String url){
         try{
-            File imgPath = new File(url);
-            BufferedImage bufferedImage = ImageIO.read(imgPath);
+            URL image = new URL(imagesLink+url+sasToken);
+            BufferedImage bufferedImage = ImageIO.read(image);
             ByteArrayOutputStream bos = new ByteArrayOutputStream();
             ImageIO.write(bufferedImage, "jpg", bos);
             return bos.toByteArray();
